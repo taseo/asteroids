836a5c9594be0c505a77cb9394f3389f43085df4
taseo <aasaadesu@protonmail.ch>
2024-12-11 23:52:18 +0200

    Feature: player
    - add CircleShape class
    - add Player class
    - draw player on screen

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
+
 from constants import *
+from entities.player import Player
 
 
 def main():
@@ -7,14 +9,34 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    updatables = pygame.sprite.Group()
+    drawables = pygame.sprite.Group()
+
+    Player.containers = (updatables, drawables)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    clock = pygame.time.Clock()
+    dt = 0
+
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         screen.fill(000000)
+
+        for updatable in updatables:
+            updatable.update(dt)
+
+        for drawable in drawables:
+            drawable.draw(screen)
+
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
     main()
+
